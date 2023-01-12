29a7349ba8470dcb21b31321e08c1cd9189a7a3e
Ken Hoff <ken@hoff.tech>
2023-01-11 20:16:11 -0700

    wip

--- build-resume.js
+++ build-resume.js
@@ -3,16 +3,21 @@ const dayjs = require("dayjs");
 
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(`file://${process.cwd()}/resume/resume.html`, {
-        waitUntil: "networkidle2"
-    });
+    try {
+        const browser = await puppeteer.launch({args: ["--no-sandbox"]});
+        const page = await browser.newPage();
+        const location = `file://${process.cwd()}/resume/resume.html`;
+        await page.goto(location, {
+            waitUntil: "networkidle2"
+        });
 
-    await page.pdf({
-        path: `build/Ken-Hoff-Resume-${dayjs().format("YYYY-MM-DD")}.pdf`,
-        printBackground: true
-    });
+        await page.pdf({
+            path: `build/Ken-Hoff-Resume-${dayjs().format("YYYY-MM-DD")}.pdf`,
+            printBackground: true
+        });
+        await browser.close();
+    } catch (e) {
+        console.log(e);
+    }
 
-    await browser.close();
 })();
